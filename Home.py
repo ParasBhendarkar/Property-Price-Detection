f1de518a56241b1a36fd8785f81242bff4074e01
Paras <147793116+ParasBhendarkar@users.noreply.github.com>
2025-11-09 19:44:31 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Real state app",
+    page_icon = ''
+)
+
+st.write('Welcome to Python Data Science')
+
+st.sidebar.success('User Input')
